f5c0a7da83a90820c836a9d77df2cb002e073cde
Aaron Eppert <aeppert@gmail.com>
2016-06-20 21:31:59 -0400

    Log properly

--- slackmsg/slackmsg.go
+++ slackmsg/slackmsg.go
@@ -7,29 +7,34 @@ import (
 )
 
 const (
-  token       = "<your token here>"
+  token = "<your token here>"
 )
 
 func usage() {
-   fmt.Println("usage: slackmsg <username> <channel> <message>");
+   fmt.Println("usage: slackmsg <username> <channel> <message>")
+}
+
+func logError(arg error) {
+  fmt.Println("Error:", arg)
+  os.Exit(1)
 }
 
 func main() {
   if len(os.Args) < 4 {
-    usage();
-    os.Exit(1);
+    usage()
+    os.Exit(1)
   }
 
   api := slack.New(token)
 
   channel, err := api.FindChannelByName(os.Args[2])
   if err != nil {
-    panic(err)
+    logError(err)
   }
 
   err = api.ChatPostMessage(channel.Id, os.Args[3], &slack.ChatPostMessageOpt{ Username: os.Args[1] })
   if err != nil {
-    panic(err)
+    logError(err)
   }
 }
 
